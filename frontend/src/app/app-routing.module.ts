b95966245adb8290817c078ae5014d1da3033e71
Ákos Mészáros <winnerduo@gmail.com>
2023-05-23 00:02:10 +0200

    alap komponensek létrehozva

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { VehiclesComponent } from './vehicles/vehicles.component';
+import { DriversComponent } from './drivers/drivers.component';
+import { TripsComponent } from './trips/trips.component';
 
-const routes: Routes = [{ path: "vehicles", component: VehiclesComponent }];
+const routes: Routes = [
+  { path: "vehicles", component: VehiclesComponent },
+  { path: "drivers", component: DriversComponent },
+  { path: "trips", component: TripsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DriversComponent } from './drivers/drivers.component';
+import { TripsComponent } from './trips/trips.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VehiclesComponent
+    VehiclesComponent,
+    NavigationComponent,
+    DriversComponent,
+    TripsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
